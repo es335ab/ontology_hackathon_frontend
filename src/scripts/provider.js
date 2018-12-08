bb7f55dd35ce4ed9e6cf3837fc5b72775a317605
es335ab <es335ab@gmail.com>
2018-12-09 00:27:34 +0900

    htmlをがっと追加

--- src/scripts/provider.js
+++ src/scripts/provider.js
@@ -1,18 +1,18 @@
 var client = dApi.client;
 client.registerClient({});
 
-var contract = '39f3fb644842c808828817bd73da0946d99f237f';
-var method = 'Hello';
-var parameters = [{ type: 'Integer', value: 5 }, { type: 'Integer', value: 4 }];
-var gasPrice = 500;
-var gasLimit = 30000;
+// var contract = '39f3fb644842c808828817bd73da0946d99f237f';
+// var method = 'Hello';
+// var parameters = [{ type: 'Integer', value: 5 }, { type: 'Integer', value: 4 }];
+// var gasPrice = 500;
+// var gasLimit = 30000;
 
-client.api.smartContract.invoke({
-  contract: contract,
-  method: method,
-  parameters: parameters,
-  gasPrice: gasPrice,
-  gasLimit: gasLimit
-}).then(function(res) {
-  console.log(res);
-});
+// client.api.smartContract.invoke({
+//   contract: contract,
+//   method: method,
+//   parameters: parameters,
+//   gasPrice: gasPrice,
+//   gasLimit: gasLimit
+// }).then(function(res) {
+//   console.log(res);
+// });
