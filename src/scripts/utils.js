e1ec2f47a50b452523f68ec49db3b0cbe6e7d3bc
es335ab <es335ab@gmail.com>
2018-12-08 23:11:43 +0900

    add util method

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,5 +1,18 @@
 var utils = {
-  test: function() {
+  urlParams: function() {
+    var params = {};
+    var paramsString = location.href.match(/\?(.*)$/);
+    var queries, i, count, query;
 
+    if (paramsString) {
+      queries = paramsString[1].split('&');
+
+      for (i = 0, count = queries.length; i < count; i++) {
+        query = queries[i].split('=');
+        params[decodeURIComponent(query[0])] = decodeURIComponent(query[1]);
+      }
+    }
+
+    return params;
   }
 };
