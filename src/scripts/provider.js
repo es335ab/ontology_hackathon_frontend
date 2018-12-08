aa3ec73e4f84ca0032aea6c008db8e10488145a0
es335ab <es335ab@gmail.com>
2018-12-08 20:14:57 +0900

    wip

--- src/scripts/provider.js
+++ src/scripts/provider.js
@@ -1,6 +1,19 @@
 var client = dApi.client;
 client.registerClient({});
 
-client.api.network.getNetwork().then(function(res) {
-  console.log(res)
+var contract = '39f3fb644842c808828817bd73da0946d99f237f';
+var method = 'Hello';
+var parameters = [{ type: 'Integer', value: 5 }, { type: 'Integer', value: 4 }];
+var gasPrice = 500;
+var gasLimit = 30000;
+
+client.api.smartContract.invoke({
+  contract: contract,
+  method: method,
+  parameters: parameters,
+  gasPrice: gasPrice,
+  gasLimit: gasLimit
+}).then(function(res) {
+  console.log(res);
+  debugger;
 });
