6976627d25aa09ae74ccf6af681a926575f92562
Jun Katagiri <katagiri.jun2@gmail.com>
2018-12-09 09:09:12 +0900

    Added: SendTokenAndMessage and ShowTransactions example

--- src/scripts/provider.js
+++ src/scripts/provider.js
@@ -1,18 +1,36 @@
 var client = dApi.client;
 client.registerClient({});
 
-// var contract = '39f3fb644842c808828817bd73da0946d99f237f';
-// var method = 'Hello';
-// var parameters = [{ type: 'Integer', value: 5 }, { type: 'Integer', value: 4 }];
-// var gasPrice = 500;
-// var gasLimit = 30000;
+client.api.asset.getAccount().then(sender => {
+  var contract = 'e03d5351186fd7b73ee74e131c466b6b74cfd01e';
+  var method = 'SendTokenAndMessage';
+  var receiver = 'ASA4WmuuqFx63zmBsXiEF1N3pCz9cTCgsU';
+  var parameters = [
+    {type: 'ByteArray', value: client.api.utils.addressToHex(sender)},
+    {type: 'ByteArray', value: client.api.utils.addressToHex(receiver)},
+    {type: 'Integer', value: 1000},
+    {type: 'String', value: "message!"}];
+  var gasPrice = 500;
+  var gasLimit = 30000;
+  client.api.smartContract.invoke({
+    contract: contract,
+    method: method,
+    parameters: parameters,
+    gasPrice: gasPrice,
+    gasLimit: gasLimit
+  }).then(function (res) {
+    console.log(res);
+  });
+});
 
-// client.api.smartContract.invoke({
-//   contract: contract,
-//   method: method,
-//   parameters: parameters,
-//   gasPrice: gasPrice,
-//   gasLimit: gasLimit
-// }).then(function(res) {
-//   console.log(res);
-// });
+var contract = 'e03d5351186fd7b73ee74e131c466b6b74cfd01e';
+var method = 'ShowTransactions';
+var childminder = 'ASA4WmuuqFx63zmBsXiEF1N3pCz9cTCgsU';
+var parameters = [{type: 'ByteArray', value: client.api.utils.addressToHex(childminder)}];
+client.api.smartContract.invokeRead({
+  contract: contract,
+  method: method,
+  parameters: parameters,
+}).then(function (res) {
+  console.log(res);
+});
